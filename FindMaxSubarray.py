345020da28fd7c789f7bc7eff54a7d19426b30c3
lancelotxly <1017010311@njupt.edu.cn>
2018-10-23 15:27:40 +0800

    matrix square multiply

--- FindMaxSubarray.py
+++ FindMaxSubarray.py
@@ -15,29 +15,30 @@ Divide and conquer method: the max_subarray must be in
 '''
 def Find_Max_Crossing_Subarray(A,low,mid,high):
     left_sum, right_sum = -Inf, -Inf
-    max_left = max_right = 0
+    max_low, max_high = None, None
     sum = 0
     for i in range(mid,low+1,-1):
         sum = sum + A[i]
         if sum > left_sum:
             left_sum = sum
-            max_left = i
+            max_low = i
+
     sum = 0
-    for i in range(mid+1, high+1, 1):
+    for i in range(mid+1,high+1):
         sum = sum + A[i]
         if sum > right_sum:
             right_sum = sum
-            max_right = i
-    max = right_sum + left_sum
-    return max_left, max_right, max
+            max_high = i
 
-def Find_Max_Subarray(A, low, high):
+    return max_low, max_high, left_sum + right_sum
+
+def Find_Max_Subarray(A,low,high):
     if low == high:
         return low, high, A[low]
     else:
         mid = floor((low+high)/2)
-        left_low, left_high, left_sum = Find_Max_Subarray(A, low, mid)
-        right_low, right_high, right_sum = Find_Max_Subarray(A, mid+1, high)
+        left_low, left_high, left_sum = Find_Max_Subarray(A,low,mid)
+        right_low, right_high, right_sum = Find_Max_Subarray(A,mid+1,high)
         cross_low, cross_high, cross_sum = Find_Max_Crossing_Subarray(A, low, mid, high)
 
     if left_sum >= right_sum and left_sum >= cross_sum:
@@ -52,44 +53,44 @@ Brute force algorithm: select the max subarray from all combination of the array
                        time-complexity: O(n^2)
 '''
 def Brute_Force_Algorithm(A):
-    left, right = 0, 0
+    left, right = None, None
     curSum = 0
     max = A[0]
     for i in range(0,len(A)):
         curSum = 0
-        for j in range(i, len(A)):
+        for j in range(i,len(A)):
             curSum = curSum + A[j]
             if curSum > max:
                 max = curSum
                 left = i
                 right = j
     return left, right, max
-
 '''
 Linear time max subarray: if max_subarray 'A_sub' of A[1,..,j] is known, the max_subarray of A[1,...,j,j+1] must be 
                           'A_sub' or some subarray A[i,..j,j+1] (1<=i<=j+1)
                           time-complexity: O(n)  
 '''
 def Linear_Time_Max_Subarray(A):
-    Max = -Inf
-    low_Max, high_Max = None, None
+    max = -Inf
+    low, high = None, None
     sum = 0
-    low_sum = 0
+    low_r = 0
     for i in range(0, len(A)):
         sum = sum + A[i]
-        if sum > Max:
-            low_Max = low_sum
-            high_Max = i
-            Max = sum
+        if sum > max:
+            max = sum
+            low = low_r
+            high = i
         if sum < 0:
             sum = 0
-            low_sum = i + 1
-    return low_Max, high_Max, Max
+            low_r = i + 1
+    return low, high, max
+
 #test
 A = [13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
 #A = [-5,-3,-4,-8]
 #l,r,s = Find_Max_Subarray(A,0,len(A)-1)
-# l,r,s = Brute_Force_Algorithm(A)
+#l,r,s = Brute_Force_Algorithm(A)
 l, r, s = Linear_Time_Max_Subarray(A)
 print(l,r,s)
 
--- MatirxMultiply.py
+++ MatirxMultiply.py
@@ -8,38 +8,52 @@ class Matrix_Define_Error(Exception):
         return 'Please input right square matrix'
 
 class Matrix():
-    def __init__(self,n, *args, zero=False):
+    def __init__(self,n,list):
         try:
-            if len(args) != n*n and zero == False:
+            if len(list) != n*n:
                 raise Matrix_Define_Error
-            elif zero == False:
-                for i in range(0,n):
-                      self.__dict__[i] = list(args[i*n:i*n+n])
             else:
-                pass
+                for i in range(0, n):
+                    self.__dict__[i] = list[i*n:i*n+n]
+                self.rows = n
         except Matrix_Define_Error as e:
             print(e)
+
+    def __setitem__(self, key1, key2, value):
+        self.__dict__[key1][key2] = value
 
     def __getitem__(self, item):
         return self.__dict__[item]
 
-    def __setitem__(self, key, key2, value):
-        self.__dict__[key][key2] = value
-
     def __call__(self, *args, **kwargs):
-        for i in range(0,len(self.__dict__)):
-            for j in range(0,len(self.__dict__[0])):
-                print(self[i][j],end=' ')
-                if j == len(self.__dict__[0])-1:
+        for i in range(0,self.rows):
+            for j in range(0, self.rows):
+                print(self[i][j], end=' ')
+                if j == self.rows - 1:
                     print()
-#test
-m = Matrix(3,1,2,3,4,5,6,7,8,9)
-m[0][0]=-1
-m()
 
+#test
+# A = list(range(1,10))
+# m = Matrix(3,A)
+# for i in range(0,3):
+#     for j in range(0,3):
+#         m[i][j] = 1
+# print(m.__dict__)
 
 def Square_Matrix_Multiply(A,B):
-    n = len(A[0])
+    n = A.rows
+    list = [0]*(n*n)  # generate zero vector
+    C = Matrix(n,list)
+    for i in range(0, n):
+        for j in range(0, n):
+            C[i][j] = 0
+            for k in range(0,n):
+                C[i][j] = C[i][j]+A[i][k]*B[k][j]
+
+    return C
 
 
-Square_Matrix_Multiply(m,m)
+A = Matrix(3,list(range(1,10)))
+B = Matrix(3, list(range(1,10)))
+C = Square_Matrix_Multiply(A, B)
+print(A(),B(),C())
