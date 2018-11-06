76e9e4df2cc31bfb5f9788a8c4f5056a474a5b5d
lancelotxly <1017010311@njupt.edu.cn>
2018-11-06 19:27:51 +0800

    build a Queue

--- Queue.py
+++ Queue.py
@@ -3,12 +3,16 @@ class Queue_Error(Exception):
         print('Queue overflow')
     def Queue_Undeflow(self):
         print('Queue underflow')
-
+'''
+Queue Dict: using this method will leave a empty space, because to verify the fullness of the queue
+            we use this condition: q.head == q.tail+1, but at this time q.tail have no data to input, so it is a empty space
+            to avoid this problem, we let the q.head = q.tail = 0 not '1', to make the length of Queue is 'n' 
+'''
 class Queue_Dict():
     def __init__(self,n):
-        self.queuelength = n
-        self.head = 1
-        self.tail = 1
+        self.QueueLength = n
+        self.head = 0
+        self.tail = 0
 
     def __setitem__(self, key, value):
         self.__dict__[key] = value
@@ -23,8 +27,41 @@ class Queue_Dict():
             return False
 
     def isQueueFull(self):
-        if self.head == self.tail + 1:
+        if (self.head == 0 and self.tail == self.QueueLength) or (self.head == self.tail + 1):
             return True
         else:
             return False
 
+    def EnQueue(self,x):
+        try:
+            if self.isQueueFull():
+                raise Queue_Error
+            else:
+                self[self.tail] = x
+                if self.tail == self.QueueLength:
+                    self.tail = 0
+                else:
+                    self.tail = self.tail + 1
+        except Queue_Error as e:
+            e.Queue_Overflow()
+
+    def DeQueue(self):
+        try:
+            if self.isQueueEmpty():
+                raise Queue_Error
+            else:
+                x = self[self.head]
+                if self.head == self.QueueLength:
+                    self.head = 0
+                else:
+                    self.head = self.head + 1
+                return x
+        except Queue_Error as e:
+            e.Queue_Undeflow()
+
+q = Queue_Dict(5)
+for i in range(0,q.QueueLength):
+    q.EnQueue(i)
+for i in range(0,q.QueueLength):
+    print(q.DeQueue())
+print(q.__dict__)
